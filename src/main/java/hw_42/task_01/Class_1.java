2ec8c07edfc072188d37eab413f5414b424bc89c
VolodymyrSinitsyn555 <Vladiofun555@gmail.com>
2024-09-06 15:53:59 +0200

    task_01

---
+++ src/main/java/hw_42/task_01/Class_1.java
@@ -0,0 +1,8 @@
+package hw_42.task_01;
+
+public class Class_1 {
+    public static void main(String[] args) {
+
+        System.out.println(" I Love GitHub");
+    }
+}
---
+++ src/main/java/hw_42/task_01/Main.java
@@ -0,0 +1,8 @@
+package hw_42.task_01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("I Love Cookies");
+    }
+}
