c4d61ccf0b48f1bea96ce3fde750569e9fc179ce
VolodymyrSinitsyn555 <Vladiofun555@gmail.com>
2024-09-06 16:12:55 +0200

    task_01

---
+++ src/main/java/hw_42/task_02/Class_4.java
@@ -0,0 +1,9 @@
+package hw_42.task_02;
+
+public class Class_4 {
+    public static void main(String[] args) {
+
+        System.out.println(" WoW");
+    }
+
+}
---
+++ src/main/java/hw_42/task_02/Main.java
@@ -0,0 +1,8 @@
+package hw_42.task_02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("PoP");
+    }
+}
